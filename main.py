0691b4055a9afe792969f748af33b082e0905684
Theo Gill <theogill1369@gmail.com>
2022-10-01 18:59:53 -0400

    Fixed and altered logic to fit game play + working how to get winner option and display winner after 2 rounds

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ from player import Player
 play = NullPlanet()
 
 play.start_game()
+
+play.play_game(round_winner=play.select_player)
+
+play.round_winner()
+
+play.game_winner()
+
+play.wins()
+
+play.looses()
--- null_planet.py
+++ null_planet.py
@@ -46,17 +46,54 @@ class NullPlanet:
             print("Invalid entry, stop mashing the keys! :P")
             self.player_name()
 
-        ## Game being played ###
+        ## Game being played znd Round Winner ###
 
-    def play_game(self):
-        while self.player_one.wins < 2 and self.player_two.wins < 2:
+    def play_game(self, round_winner):
+        self.wins = 0
+        self.looses = 0
+        while self.player_one.wins < 2 and self.player_two.wins < 2 and self.player_one.looses > 1 and self.player_two.looses > 1 :
             player_one_choice = self.player_one.throw_gesture()
             player_two_choice = self.player_two.throw_gesture()
             print(f'Player One Gesture: {player_one_choice}')
             print(f'Player Two Gesture: {player_two_choice}')
+            return self.play_game()
+        if self.player_one.wins == 1:
+            print("You won that round")
+            self.wins += 1
+            return round_winner
+        elif self.player_two.looses == 1:
+            print("Oh no, you lost that round")
+            self.looses += 1
+            return round_winner
+        elif self.player_two.wins == 1:
+            print("Good Job, you won this round!")
+            self.wins += 1
+            return round_winner
+        elif self.player_two.looses == 1:
+            print("Ouch, I hate when that happens")
+            self.looses += 1
+            return round_winner(self.wins and self.looses)
+        print(round_winner)
+        
+
+            ## Round winner ##
+    def round_winner(self):
+        self.wins = 0
+        self.looses = 0
+
+    def game_winner(self):
+        pass
+
+    def wins(self):
+        self.wins += 1
+
+    def looses(self):
+        self.looses += 1
 
     def start_game(self):
         self.welcome_message()
         self.select_player()
         self.player_name()
-        self.play_game()
+        self.play_game(round_winner=self.select_player)
+        self.round_winner()
+        self.game_winner()
